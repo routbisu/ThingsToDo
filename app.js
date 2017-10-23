c01ec3b3271e9f544427bd78eae757475537f4d8
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2017-10-23 23:02:47 +0530

    Added JS code

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //chrome.browserAction.setBadgeText({ text: "" });
+// Global values
+
+$(document).ready(function() { 
+    //$('.sidebar').hide();
+    // Handle hamburger menu
+    $('#hamburger-menu').click(function() {
+        //$('.sidebar').removeClass('hidden');
+        $('.sidebar').addClass('visible-menu');        
+    })
+});
