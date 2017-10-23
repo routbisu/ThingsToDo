53e62df3d70f6cef1ab8370fb00808e592472542
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2017-10-24 00:16:18 +0530

    Added JS code

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 //chrome.browserAction.setBadgeText({ text: "" });
 // Global values
+var isMenuOpen = false;
 
 $(document).ready(function() { 
     //$('.sidebar').hide();
     // Handle hamburger menu
-    $('#hamburger-menu').click(function() {
+    $('header').click(function(evt) {
+        evt.stopPropagation();
         //$('.sidebar').removeClass('hidden');
-        $('.sidebar').addClass('visible-menu');        
-    })
+        if(isMenuOpen) {
+            $('#sidebar-content').hide();
+            $('.sidebar').removeClass('visible-menu');
+            $('.sidebar').addClass('hidden-menu');
+            $('#hamburger-menu').removeClass('fa-times');            
+            $('#hamburger-menu').addClass('fa-bars');
+        } else {
+            $('.sidebar').removeClass('hidden-menu');
+            $('.sidebar').addClass('visible-menu');    
+            $('#sidebar-content').show();        
+            $('#hamburger-menu').removeClass('fa-bars');
+            $('#hamburger-menu').addClass('fa-times');
+        }     
+        isMenuOpen = !isMenuOpen;
+    });
+
+    // Stop propagation for clicks on sidebar
+    $('.sidebar').click(function(evt) {
+        evt.stopPropagation();
+    });
+
+    // Handle click outside to close menu
+    $(document).click(function() {
+        $('#sidebar-content').hide();
+        $('.sidebar').removeClass('visible-menu');
+        $('.sidebar').addClass('hidden-menu');
+        $('#hamburger-menu').removeClass('fa-times');            
+        $('#hamburger-menu').addClass('fa-bars');
+        isMenuOpen = !isMenuOpen;
+    });
 });
